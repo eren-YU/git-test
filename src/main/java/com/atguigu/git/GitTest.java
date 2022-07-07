86f4ecc9ca017fcd3d3e9fa1d6eaef8b4c0d1572
eren <erenyu@163.com>
2022-07-07 15:40:35 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @Author: eren
+ * @Date: 2022/7/7 - 15:33
+ * @Description:
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
